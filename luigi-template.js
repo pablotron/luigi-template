3e4c0bf17850b73e8916186b449bb41d21d255c6
pabs@pablotron.org <pabs@pablotron.org>
2014-12-17 23:50:03 -0500

    update documentation

--- luigi-template.js
+++ luigi-template.js
@@ -14,7 +14,7 @@
  *
  * Why?  This script is:
  *
- *   * less than 3k minified (see `forced-timeout.min.js`),
+ *   * less than 3k minified (see `luigi-template.min.js`),
  *
  *   * has no external dependencies (no jQuery/YUI/Sensha),
  *
