dfc94a5739c99ed0e307fefaeb0dd612a14e8e03
pabs@pablotron.org <pabs@pablotron.org>
2014-12-18 16:04:12 -0500

    add custom and custom args tests

--- java/pablotron/luigi/Test.java
+++ java/pablotron/luigi/Test.java
@@ -2,19 +2,41 @@ package pablotron.luigi;
 
 import java.util.Map;
 import java.util.HashMap;
-import pablotron.luigi.Template;
 import pablotron.luigi.LuigiError;
+import pablotron.luigi.Template;
+import pablotron.luigi.Filter;
 
 public final class Test {
-  private static final String TEMPLATE = "hello %{name | uc}";
+  private static final String TEMPLATE =
+    "test basic: hello %{name}\n" +
+    "test filter: hello %{name | uc}\n" +
+    "test custom: %{name | custom | uc | lc}\n" +
+    "test custom_with_arg: %{name | custom_with_arg hello}\n";
 
   private static final Map<String, String> args = new HashMap<String, String>() {{
     put("name", "paul");
   }};
 
-  public static void main(String cli_args[]) throws Exception {
-    final Template t = new Template(TEMPLATE);
+  private static final Map<String, Filter.Handler> filters = new HashMap<String, Filter.Handler>() {{
+    put("custom", new Filter.Handler() {
+      public String filter(String val, String args[], Map<String, String> row) {
+        return "custom";
+      }
+    });
 
-    System.out.println(t.run(args));
+    put("custom_with_arg", new Filter.Handler() {
+      public String filter(String val, String args[], Map<String, String> row) {
+        return (args.length > 0) ? args[0] : "custom";
+      }
+    });
+  }};
+
+  public static void main(String cli_args[]) throws Exception {
+    // add custom filters to default filter cache
+    Filter.FILTERS.putAll(filters);
+
+    // create and run template
+    final Template t = new Template(TEMPLATE);
+    System.out.print(t.run(args));
   }
 };
