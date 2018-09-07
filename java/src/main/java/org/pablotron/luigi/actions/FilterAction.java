163e6cf03d369f0b062c8aa19fb26c5892bc2b05
Paul Duncan <pabs@pablotron.org>
2018-09-06 23:02:52 -0400

    java: document actions

--- java/src/main/java/org/pablotron/luigi/actions/FilterAction.java
+++ java/src/main/java/org/pablotron/luigi/actions/FilterAction.java
@@ -1,6 +1,7 @@
 package org.pablotron.luigi.actions;
 
 import java.util.Map;
+
 import org.pablotron.luigi.actions.Action;
 import org.pablotron.luigi.FilterReference;
 import org.pablotron.luigi.Filter;
@@ -8,15 +9,41 @@ import org.pablotron.luigi.errors.LuigiError;
 import org.pablotron.luigi.errors.UnknownFilterError;
 import org.pablotron.luigi.errors.UnknownKeyError;
 
+/**
+ * Action that expands the given key and applies any specified filters.
+ */
 public final class FilterAction implements Action {
+  /**
+   * Value key.
+   */
   private final String key;
+
+  /**
+   * Array of filters to apply to value.
+   */
   private final FilterReference filters[];
 
+  /**
+   * Create a new FilterAction.
+   *
+   * @param key Template run argument key.
+   * @param filters Array of filter references.
+   */
   public FilterAction(final String key, final FilterReference filters[]) {
     this.key = key;
     this.filters = filters;
   }
 
+  /**
+   * Run this filter action and return the result as a string.
+   *
+   * @param filters Template filters.
+   * @param key Template run arguments.
+   *
+   * @return Action result.
+   *
+   * @throws LuigiError If an error occured while running this action.
+   */
   public String run(
     Map<String, Filter.Handler> filters,
     Map<String, String> args
@@ -33,7 +60,7 @@ public final class FilterAction implements Action {
       if (f == null)
         throw new UnknownFilterError(this.filters[i].name);
 
-      // run filter 
+      // run filter
       r = f.filter(r, this.filters[i].args, args);
     }
 
